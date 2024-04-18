44be9127005caac5cae724f867bd285ab8bb8403
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-04-18 07:38:36 +0000

    [Add]コメント機能 いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: :about
-  resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :post_images, only: [:new, :index, :show, :create, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorite, only: [:create, :destroy]
+  end   
   resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
